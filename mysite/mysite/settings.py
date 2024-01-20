c66b39122b4666cf5b4b663c52f95e89c0f7832d
edvin <edvinkeskin@gmail.com>
2024-01-20 14:16:04 -0800

    added init files, didn't setup mongodb yet

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django',
+        'NAME': 'your_mongodb_database',
     }
 }
 
