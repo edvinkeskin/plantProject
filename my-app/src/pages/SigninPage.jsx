5a68746afd6927a69b9daba5edbd4017125dec74
angadhsingh1 <angadh.singh1@gmail.com>
2024-01-21 01:07:26 -0800

    changes to login user feature

--- my-app/src/pages/SigninPage.jsx
+++ my-app/src/pages/SigninPage.jsx
@@ -98,7 +98,7 @@ export default function SignIn() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="http://localhost:3000/signup" variant="body2">
+                <Link href="http://localhost:3000/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
