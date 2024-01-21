11d26063fb30720cc267099907fc878e81795c13
Richard Lee <richardzhyulee@gmail.com>
2024-01-21 04:07:33 -0800

    Refactored Image

--- my-app/src/components/Produce.jsx
+++ my-app/src/components/Produce.jsx
@@ -13,7 +13,12 @@ const Product = ({ produce }) => {
     return (
         <Container>
             <Card onClick={handleOpenModal}>
-                <Card.Img variant="top" src={`http://localhost:8000${produce.image}`} alt="Ugly Potato"/>
+                <Card.Img
+                    className="maxImage"
+                    variant="top"
+                    src={`http://localhost:8000${produce.image}`}
+                    alt="Ugly Potato"
+                />
                 <Card.Body>
                     <Card.Title>{produce.name}</Card.Title>
                     <Card.Text className="m-0">Price: ${produce.price}</Card.Text>
@@ -25,7 +30,13 @@ const Product = ({ produce }) => {
                 <Modal.Body>
                     <Row>
                         <Col md={12} lg={6}>
-                            <Image src={`http://localhost:8000${produce.image}`} alt={produce.name} rounded fluid/>
+                            <Image
+                                src={`http://localhost:8000${produce.image}`}
+                                alt={produce.name}
+                                rounded
+                                fluid
+                                style={{ width: '540px', height: '400px' }}
+                             />
                         </Col>
                         <Col md={12} lg={6}>
                             <div className="my-2 d-flex flex-column justify-content-center">
