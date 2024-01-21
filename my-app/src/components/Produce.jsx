7fad41fd0cc8b4664645ab54957241ee92a2a282
Richard Lee <richardzhyulee@gmail.com>
2024-01-20 17:04:43 -0800

    Updated Produce Component

--- my-app/src/components/Produce.jsx
+++ my-app/src/components/Produce.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Card, Container, Image, Modal} from "react-bootstrap";
+import {Button, Card, Container, Image, Modal} from "react-bootstrap";
 
 const Product = () => {
     const [showModal, setShowModal] = useState(false);
@@ -14,36 +14,36 @@ const Product = () => {
                 <Card.Img variant="top" src={imagePath} alt="Ugly Potato"/>
                 <Card.Body>
                     <Card.Title>Potato</Card.Title>
-                    <Card.Text className="m-0">Price: $100.00</Card.Text>
+                    <Card.Text className="m-0">Price: $10.00</Card.Text>
                     <Card.Text>Expires: 01-01-2024</Card.Text>
                 </Card.Body>
             </Card>
-
-            // Seller
-            // Product Name
-            // Price
-            // Quantity
-
-            // Description
-
-            // Contact Buyer
-
-            <Modal show={showModal} onHide={handleCloseModal} size="xl">
+            <Modal show={showModal} onHide={handleCloseModal} size="xl" centered>
                 <Modal.Header closeButton/>
                 <Modal.Body>
                     <div className="d-flex flex-row">
                         <Image src={imagePath} alt="Ugly Potato" rounded/>
-                        <div className="ps-3">
-                            <h1>Potato</h1>
-                            <p>Price: $100.00</p>
-                            <p>Expires: 01-01-2024</p>
-                            <p>Despite its unconventional appearance, the Ugly Potato boasts a hearty and earthy taste
-                                that adds depth to a variety of dishes.
-                            </p>
+                        <div className="ps-3 d-flex flex-column justify-content-center">
+                            <div>
+                                <h1>Potato</h1>
+                                <p className="m-0 p-0">Price: $10.00</p>
+                                <p>Expires: 2024-01-01</p>
+                                <p className="m-0 p-0">
+                                    Despite its unconventional appearance, the Ugly Potato boasts a hearty and earthy
+                                    taste that adds depth to a variety of dishes.
+                                </p>
+                            </div>
                             <hr/>
-                            <p>Seller Information</p>
-                            <p>Jackson Wang</p>
-                            <p>Vancouver, British Columbia</p>
+                            <div>
+                                <h5>Seller Information</h5>
+                                <p className="m-0 p-0">Richard Lee</p>
+                                <p className="m-0 p-0">Vancouver, BC Canada</p>
+                            </div>
+                            <hr/>
+                            <div>
+                                <h5>Contact Seller</h5>
+                                <Button className="w-100">Send</Button>
+                            </div>
                         </div>
                     </div>
                 </Modal.Body>
