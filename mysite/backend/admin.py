f8e7122b65a3939f4d24bc28dffa096a2d4800f6
TylerWon <won.tyler1@gmail.com>
2024-01-20 16:54:44 -0800

    serve media files

--- mysite/backend/admin.py
+++ mysite/backend/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.listing import Listing
+
+admin.site.register(Listing)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("list/", )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # for serving media files
